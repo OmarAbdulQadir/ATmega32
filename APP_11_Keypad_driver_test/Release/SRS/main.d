846199cb391e2827f20123850ee376eeb258634d
OmarAbdulQadir <oabdulqadir2@gmail.com>
2023-06-30 02:04:08 +0300

    Adding app 11 testing keypad driver

---
+++ APP_11_Keypad_driver_test/Release/SRS/main.d
@@ -0,0 +1,12 @@
+SRS/main.o SRS/main.o: ../SRS/main.c ../SRS/../../LIB/BIT_MATH.h \
+  ../SRS/../../LIB/STD_TYPES.h \
+  ../SRS/../../Drivers/MCAL/DIO/DIO_interface.h \
+  ../SRS/../../Drivers/HAL/KEYPAD/KEYPAD_interface.h
+
+../SRS/../../LIB/BIT_MATH.h:
+
+../SRS/../../LIB/STD_TYPES.h:
+
+../SRS/../../Drivers/MCAL/DIO/DIO_interface.h:
+
+../SRS/../../Drivers/HAL/KEYPAD/KEYPAD_interface.h:
---
+++ APP_11_Keypad_driver_test/SRS/main.c
@@ -0,0 +1,41 @@
+/*
+ * main.c
+ *
+ *  Created on: May 12, 2023
+ *      Author: Omar A.Qadir
+ */
+
+
+//Standard libraries Section
+//#include <avr/io.h>
+#include <util/delay.h>
+#include "../../LIB/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+
+//MCAL layer Section
+//#include "../../MCAL/<driverName>/interface.h"
+#include "../../Drivers/MCAL/DIO/DIO_interface.h"
+
+//HAL layer Section
+//#include "../../Drivers/HAL/<driverName>/<driverName>_interface.h"
+#include "../../Drivers/HAL/KEYPAD/KEYPAD_interface.h"
+
+//Private libraries Section
+//#include "../INC/<fileName>.h"
+
+//Global valriables, struct and enums
+
+
+
+int main(void){
+	DIO_void_set_port_dir(PORTC, OUTPUT);
+	keypad_init();
+	s8 val_id;
+
+	while(1){
+		val_id = keypad_read();
+		if(-1 != val_id)
+			DIO_void_assign_port(PORTC, val_id);
+	}
+	return 0;
+}
