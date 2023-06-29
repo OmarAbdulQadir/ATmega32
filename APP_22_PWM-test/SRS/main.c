b637ce140b772a75b423ea9b4022c26371e568c3
OmarAbdulQadir <oabdulqadir2@gmail.com>
2023-06-30 02:50:25 +0300

    Adding app 22 PWM driver testing

---
+++ APP_22_PWM-test/SRS/main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 10, 2023
+ *      Author: Omar A.Qadir
+ */
+
+
+// Standard libraries Section
+#include <util/delay.h>
+#include "../../LIB/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+
+// Drivers Section
+
+
+// Functions decleration
+
+
+// Global variables section
+
+
+int main(void ){
+	// Setup section
+
+	while (1){
+		// Loop section
+
+	}
+	return 0;
+}
+
+// Functions implementation
