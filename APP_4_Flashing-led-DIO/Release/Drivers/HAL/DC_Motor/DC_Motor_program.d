33d5600f110b2a432d7cf3fc12de26ecf01becc1
OmarAbdulQadir <oabdulqadir2@gmail.com>
2023-06-30 00:58:49 +0300

    Adding app 4 Flashing led with DIO driver

---
+++ APP_4_Flashing-led-DIO/Release/Drivers/HAL/DC_Motor/DC_Motor_program.d
@@ -0,0 +1,15 @@
+Drivers/HAL/DC_Motor/DC_Motor_program.o \
+  Drivers/HAL/DC_Motor/DC_Motor_program.o:  \
+ D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/DC_Motor_program.c \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/../../../LIB/BIT_MATH.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/../../../LIB/STD_TYPES.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/../../MCAL/DIO/DIO_interface.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/DC_Motor_interface.h
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/../../../LIB/BIT_MATH.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/../../../LIB/STD_TYPES.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/../../MCAL/DIO/DIO_interface.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/DC_Motor/DC_Motor_interface.h:
