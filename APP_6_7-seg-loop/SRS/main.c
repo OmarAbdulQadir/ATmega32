905a46465442f1d1ea51da77882f84b8bdb89bb3
OmarAbdulQadir <oabdulqadir2@gmail.com>
2023-06-30 01:47:09 +0300

    Adding app 6 counting from 0-9 with 7 seg disp

---
+++ APP_6_7-seg-loop/SRS/main.c
@@ -0,0 +1,45 @@
+/*
+ * main.c
+ *
+ *  Created on: May 5, 2023
+ *      Author: Omar A.Qadir
+ */
+
+
+
+//Standard libraries Section
+//#include <avr/io.h>
+#include <util/delay.h>
+#include "../../LIB/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+
+//MCAL layer Section
+//#include "../MCAL/<driverName>/interface.h"
+#include "../../Drivers/MCAL/DIO/DIO_interface.h"
+
+//HAL layer Section
+//#include "../HAL/<driverName>/interface.h"
+
+//Private libraries Section
+//#include "../INC/<fileName>.h"
+
+//Global valriables, struct and enums
+#define max 9
+#define min 0
+
+int main(void){
+
+	u8 seg_val_dec[10] ={64, 121, 36, 48, 25, 18, 2, 120, 0, 24}; //DEC values for the 7 segment numbers
+	DIO_void_set_port_dir(PORTC, PORT_MAX);
+	DIO_void_set_port(PORTC);
+	u8 count = min;
+
+	while(1){
+		if (max < count){
+			count = min;
+		}
+		DIO_void_assign_port(PORTC, seg_val_dec[count++]);
+		_delay_ms(1000);
+	}
+	return 0;
+}
