12ea8cc27feca2a2724abf96d615759a5dde85b4
OmarAbdulQadir <oabdulqadir2@gmail.com>
2023-06-30 02:24:25 +0300

    updating app 13, 12

--- APP_12_LCD-test/Debug/SRS/main.d
+++
@@ -1,9 +0,0 @@
-SRS/main.o SRS/main.o: ../SRS/main.c ../SRS/../../LIB/STD_TYPES.h \
-  ../SRS/../../LIB/BIT_MATH.h \
-  ../SRS/../../Drivers/MCAL/DIO/DIO_interface.h
-
-../SRS/../../LIB/STD_TYPES.h:
-
-../SRS/../../LIB/BIT_MATH.h:
-
-../SRS/../../Drivers/MCAL/DIO/DIO_interface.h:
