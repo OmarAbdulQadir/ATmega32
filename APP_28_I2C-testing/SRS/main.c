adf7348ec05bd202d972285b32fa8947af03001b
OmarAbdulQadir <oabdulqadir2@gmail.com>
2023-06-30 02:55:30 +0300

    Adding app 28 I2C Driver testing

---
+++ APP_28_I2C-testing/SRS/main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 23, 2023
+ *      Author: Omar A.Qadir
+ */
+
+
+// Libraries section
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+
+// Driver section
+
+
+// Private section
+
+
+// Macros section
+
+
+// Global variables section
+
+
+// Main app
+int main(void ){
+	// Setup section
+
+	while(1){
+		// Loop section
+
+	}
+}
