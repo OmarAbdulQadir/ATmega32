a73b211c25f3e2130416d3627f21379d7d4e605f
Omar Abdul Qadir <oabdulqadir2@gmail.com>
2023-06-30 02:24:31 +0300

    Adding app 12 lcd testing with DIO

---
+++ APP_12_LCD-test/Release/Drivers/HAL/LCD_8bit/LCD_8bit_program.d
@@ -0,0 +1,21 @@
+Drivers/HAL/LCD_8bit/LCD_8bit_program.o \
+  Drivers/HAL/LCD_8bit/LCD_8bit_program.o:  \
+ D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_program.c \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/../../../LIB/BIT_MATH.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/../../../LIB/STD_TYPES.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/../../MCAL/DIO/DIO_interface.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_config.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_private.h \
+  D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_interface.h
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/../../../LIB/BIT_MATH.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/../../../LIB/STD_TYPES.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/../../MCAL/DIO/DIO_interface.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_config.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_private.h:
+
+D:/Engineering/Embedded/IMT_Dip/Interfacing/Workplace/Drivers/HAL/LCD_8bit/LCD_8bit_interface.h:
---
+++ APP_12_LCD-test/Release/SRS/main.d
@@ -0,0 +1,9 @@
+SRS/main.o SRS/main.o: ../SRS/main.c ../SRS/../../LIB/STD_TYPES.h \
+  ../SRS/../../LIB/BIT_MATH.h \
+  ../SRS/../../Drivers/MCAL/DIO/DIO_interface.h
+
+../SRS/../../LIB/STD_TYPES.h:
+
+../SRS/../../LIB/BIT_MATH.h:
+
+../SRS/../../Drivers/MCAL/DIO/DIO_interface.h:
